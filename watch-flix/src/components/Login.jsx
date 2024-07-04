de6d2926e2e369b1c2b137165996ed29a1fde1be
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-04 23:43:30 +0530

    Feat:sign up feat.

--- watch-flix/src/components/Login.jsx
+++ watch-flix/src/components/Login.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [signIN, setsignIN] = useState(true);
   return (
     <>
       <section className=" w-full h-screen bg-gradient-to-t  from-black pt-8">
         <form className="w-full max-w-xs sm:max-w-md   p-12 bg-opacity-80 bg-black  mx-auto right-0 left-0 text-white rounded-lg  ">
           <div className="flex flex-col">
+            {!signIN && (
+              <>
+                <label
+                  htmlFor="name"
+                  className=" m-2 text-lg font-semibold font-serif"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder=" Enter user Name"
+                  className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+                />
+              </>
+            )}
             <label
               htmlFor="email"
               className="m-2 text-lg font-semibold font-serif"
@@ -30,18 +49,26 @@ const Login = () => {
               placeholder="Enter password"
               className="p-4 my-4 w-full bg-gray-700 rounded-lg"
             />
-
-            <div className=" flex justify-between flex-col sm:flex-row items-center">
+            {signIN ? (
+              <div className=" flex justify-between flex-col sm:flex-row items-center">
+                <button
+                  type="submit"
+                  className="bg-red-500 rounded-2xl my-4 w-1/2 py-2 text-center "
+                >
+                  Sign In
+                </button>
+                <span className="p-2 sm:my-4 inline-block text-red-400">
+                  Forgot password ?
+                </span>
+              </div>
+            ) : (
               <button
                 type="submit"
-                className="bg-red-500 rounded-2xl my-4 w-1/2 py-2 text-center "
+                className="bg-red-500 rounded-2xl my-4 w-1/2 py-2 text-center mx-auto right-0 left-0 "
               >
-                Sign In
+                Sign up
               </button>
-              <span className="p-2 sm:my-4 inline-block text-red-400">
-                Forgot password ?
-              </span>
-            </div>
+            )}
           </div>
         </form>
       </section>

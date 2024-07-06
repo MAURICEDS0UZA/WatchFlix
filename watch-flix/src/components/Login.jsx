1e0cf63ed6bd90e6e31c0a4b320053bfc60abc9e
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-06 23:00:06 +0530

    Feat:Login page validation and used ref hook.

--- watch-flix/src/components/Login.jsx
+++ watch-flix/src/components/Login.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import validateInput from "../utils/validateForm";
 
 const Login = () => {
-  const [signIN, setsignIN] = useState(true);
+  const [signIn, setsignIn] = useState(true);
+  const [errmessage, seterrmessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const toggleSignInForm = () => {
+    setsignIn(!signIn);
+  };
+  const handleValidation = () => {
+    console.log(email.current.value);
+    const message = validateInput(email.current.value, password.current.value);
+    seterrmessage(message);
+  };
+
   return (
     <>
       <section className=" w-full h-screen bg-gradient-to-t  from-black pt-8">
-        <form className="w-full max-w-xs sm:max-w-md   p-12 bg-opacity-80 bg-black  mx-auto right-0 left-0 text-white rounded-lg  ">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="w-full max-w-xs sm:max-w-md   p-12 bg-opacity-80 bg-black  mx-auto right-0 left-0 text-white rounded-lg  "
+        >
           <div className="flex flex-col">
-            {!signIN && (
+            {!signIn && (
               <>
                 <label
                   htmlFor="name"
@@ -35,6 +53,7 @@ const Login = () => {
               id="email"
               placeholder="Enter email ID"
               className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+              ref={email}
             />
             <label
               htmlFor="password"
@@ -48,13 +67,17 @@ const Login = () => {
               id="password"
               placeholder="Enter password"
               className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+              ref={password}
             />
-            {signIN ? (
+            <p>{errmessage}</p>
+            {signIn ? (
               <div className=" flex justify-between flex-col sm:flex-row items-center">
                 <button
                   type="submit"
                   className="bg-red-500 rounded-2xl my-4 w-1/2 py-2 text-center "
-                  onClick={() => setsignIN(!signIN)}
+                  onClick={() => {
+                    handleValidation();
+                  }}
                 >
                   Sign In
                 </button>
@@ -65,11 +88,22 @@ const Login = () => {
             ) : (
               <button
                 type="submit"
+                onClick={() => {
+                  handleValidation();
+                }}
                 className="bg-red-500 rounded-2xl my-4 w-1/2 py-2 text-center mx-auto right-0 left-0 "
               >
                 Sign up
               </button>
             )}
+            <p
+              className="py-4 cursor-pointer font-mono"
+              onClick={toggleSignInForm}
+            >
+              {signIn
+                ? "New to Netflix? Sign Up Now"
+                : "Already registered? Sign In Now."}
+            </p>
           </div>
         </form>
       </section>

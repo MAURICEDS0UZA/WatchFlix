5f063c515627f10f1e965ed65368fefc5e53638a
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-17 18:02:24 +0530

    Feat:Provided routing.

--- watch-flix/src/App.js
+++ watch-flix/src/App.js
@@ -1,15 +1,57 @@
 import "./App.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
-import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import appStore from "./utils/appStore";
+import { Provider } from "react-redux";
+import Browser from "./components/Browser";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header />
+              <Outlet />
+            </>
+          }
+        >
+          <Route
+            index
+            path="/"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/Browser"
+            element={
+              <>
+                <Browser />
+              </>
+            }
+          />
+        </Route>
+      </>
+    )
+  );
   return (
     <>
       <Provider store={appStore}>
-        <Header />
-        <Login />
+        <RouterProvider router={router} />
       </Provider>
     </>
   );

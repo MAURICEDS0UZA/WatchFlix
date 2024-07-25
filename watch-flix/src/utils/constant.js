148ecd035417edbe21c3f11c0beaba5d5e88dd97
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-25 23:11:23 +0530

    Feat:Api option for fetching now playing movie

--- watch-flix/src/utils/constant.js
+++ watch-flix/src/utils/constant.js
@@ -1,2 +1,11 @@
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const Api_Option = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzN2I1ZWNmOGM0YzVlM2YwN2UwNjE5MTIwOTI1OTAyZiIsIm5iZiI6MTcyMTg4MDI4OS4xMDY2NjIsInN1YiI6IjY1YjczNGJjMGNkMzJhMDE2M2U2Njg4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dkkVku5dRYAehWCDKUFb-tnS33ukmIVtY3NcvYdXKTg",
+  },
+};

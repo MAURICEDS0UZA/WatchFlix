be062950d9a7369f5dac95b1fad9021d61926006
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-25 22:53:38 +0530

    New:Now playing hook

---
+++ watch-flix/src/hooks/useNowPlaying.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Api_Option } from "../utils/constant";
+import { addMovie } from "../utils/movieSlice";
+
+const UseNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+  const getNowPlayingMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      Api_Option
+    );
+    const data = await res.json();
+    dispatch(addMovie(data));
+  };
+};
+
+export default UseNowPlayingMovies;

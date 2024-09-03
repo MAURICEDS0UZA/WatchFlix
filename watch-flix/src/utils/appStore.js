837f63cd342e6e81074d0edb75b5e02ae140f837
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-09-03 21:08:43 +0530

    Add: Popular movies add to app store.

--- watch-flix/src/utils/appStore.js
+++ watch-flix/src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./configSlice";
 import movieReducer from "./movieSlice";
+import popularReducer from "./popular";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     movies: movieReducer,
+    popular: popularReducer,
   },
 });
 

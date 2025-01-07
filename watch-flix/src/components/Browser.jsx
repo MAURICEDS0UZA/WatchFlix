f39a54581a54acc8d5226eeda56aeeb86daeda60
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2025-01-07 23:06:34 +0530

    Add:Now palying movies & popular movies.

--- watch-flix/src/components/Browser.jsx
+++ watch-flix/src/components/Browser.jsx
@@ -1,11 +1,17 @@
 import UseNowPlayingMovies from "../hooks/useNowPlaying";
+import usePopularMovies from "../hooks/usePopularMovie";
 import MainContainer from "./MainContainer";
+import NowPlayingMovies from "./NowPlayingMovies";
+import TopRatedMovies from "./TopRatedMovies";
 //** */ i have rename file from mainContainer -Maincontainer
 const Browser = () => {
   UseNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <MainContainer />
+      <NowPlayingMovies />
+      <TopRatedMovies />
     </div>
   );
 };
